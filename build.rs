1ff23a61f7729897327c5b009a2476eb080681f8
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2024-08-26 01:07:58 +0200

    Fix linter issue

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() -> Result<()> {
     // std::env::set_var("PROTOC", "/usr/local/bin/protoc");
 
     let mut protobuf_out = std::path::PathBuf::new();
-    protobuf_out.push(&std::env::var("OUT_DIR").unwrap());
+    protobuf_out.push(std::env::var("OUT_DIR").unwrap());
     protobuf_out.push("protobuf");
     std::fs::create_dir(&protobuf_out).ok();
 
