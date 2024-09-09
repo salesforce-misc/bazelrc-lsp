5cf1f758736614c79f62e82aaeb6a6cd74f6472f
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-09-09 03:34:11 -0700

    feat: go to definition on imports

---
+++ src/definition.rs
@@ -0,0 +1,49 @@
+use std::path::Path;
+
+use tower_lsp::lsp_types::*;
+
+use crate::{file_utils::resolve_bazelrc_path, line_index::IndexEntryKind, parser::Line};
+
+pub fn get_definitions(
+    file_path: &Path,
+    kind: &IndexEntryKind,
+    line: &Line,
+) -> Option<GotoDefinitionResponse> {
+    match kind {
+        IndexEntryKind::FlagValue(flag_nr) => {
+            let flag = &line.flags[*flag_nr];
+            if Option::is_none(&line.command) {
+                return None;
+            }
+            if line.flags.len() != 1 {
+                return None;
+            }
+            let command_name = &line.command.as_ref().unwrap().0;
+            if *command_name != "import" && *command_name != "try-import" {
+                return None;
+            }
+
+            if Option::is_none(&flag.value) {
+                return None;
+            }
+
+            let flag_value = &flag.value.as_ref().unwrap().0;
+            let path = resolve_bazelrc_path(file_path, flag_value)?;
+            let url = Url::from_file_path(path).ok()?;
+            Some(GotoDefinitionResponse::Scalar(Location {
+                uri: url,
+                range: Range {
+                    start: Position {
+                        line: 0,
+                        character: 0,
+                    },
+                    end: Position {
+                        line: 0,
+                        character: 0,
+                    },
+                },
+            }))
+        }
+        _ => None,
+    }
+}
