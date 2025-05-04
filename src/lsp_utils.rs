b05e319adc96218d41f92b1432db045e814e5cfa
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2025-05-04 22:47:20 +0200

    Fix position encoding for Unicode characters
    LSP requires positions to be encoded as UTF-16 code units. However, we
    so far used of UTF-32 code points.
    
    Fixes #9

--- src/lsp_utils.rs
+++ src/lsp_utils.rs
@@ -4,14 +4,17 @@ use tower_lsp::lsp_types::{Position, Range};
 use crate::tokenizer::Span;
 
 pub fn lsp_pos_to_offset(rope: &Rope, pos: &Position) -> Option<usize> {
-    let char = rope.try_line_to_char(pos.line as usize).ok()?;
-    Some(char + pos.character as usize)
+    let line_byte = rope.try_line_to_byte(pos.line as usize).ok()?;
+    let char_byte = rope
+        .try_char_to_byte(rope.try_utf16_cu_to_char(pos.character as usize).ok()?)
+        .ok()?;
+    Some(line_byte + char_byte)
 }
 
 pub fn offset_to_lsp_pos(rope: &Rope, pos: usize) -> Option<Position> {
-    let line = rope.try_byte_to_line(pos).ok()?;
-    let first = rope.try_line_to_char(line).ok()?;
-    let character = rope.try_byte_to_char(pos).ok()? - first;
+    let line = rope.byte_to_line(pos);
+    let first = rope.char_to_utf16_cu(rope.line_to_char(line));
+    let character = rope.char_to_utf16_cu(rope.byte_to_char(pos)) - first;
     Some(Position {
         line: line.try_into().ok()?,
         character: character.try_into().ok()?,
